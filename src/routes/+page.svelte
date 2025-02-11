8e99105248425d8db1d1cd49a5339e716525f11e
TraFost <rahwisdilfiqrak@gmail.com>
2025-02-11 23:37:00 +0700

    feat: add layout & error page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,15 @@
 <script lang="ts">
-  import "$lib/styles/app.css";
+  import '$lib/styles/app.css';
 
-  import Hero from "$components/Hero.svelte";
-  import Technologies from "$components/Technologies.svelte";
-  import Projects from "$components/Projects.svelte";
-  import Experiences from "$components/Experiences.svelte"
-  import Footer from "$components/Footer.svelte";
-  import Educations from "$components/Educations.svelte";
-
-  import config from "$lib/config";
+  import Hero from '$components/Hero.svelte';
+  import Technologies from '$components/Technologies.svelte';
+  import Projects from '$components/Projects.svelte';
+  import Experiences from '$components/Experiences.svelte';
+  import Educations from '$components/Educations.svelte';
 </script>
 
-<svelte:head>
-  <title>{config.meta.title}</title>
-  <meta name="description" content={config.meta.description} />
-  <meta name="og:title" content={config.meta.title} />
-  <meta name="og:description" content={config.meta.description} />
-  <meta name="twitter:description" content={config.meta.description} />
-  <meta name="twitter:card" content="summary_large_image" />
-  <link rel="icon" href={config.meta.favicon} />
-</svelte:head>
-
-<div class="min-h-[100dvh] bg-black text-white overflow-hidden">
-  <main class="container mx-auto px-6 space-y-20">
-    <Hero />
-    <Technologies />
-    <Projects />
-    <Experiences />
-    <Educations />
-  </main>
-  <Footer />
-</div>
+<Hero/>
+<Technologies/>
+<Projects/>
+<Experiences/>
+<Educations/>
