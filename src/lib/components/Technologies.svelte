6a481a0338235d49576e31d5a2b1abb4d3e64a90
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-10-01 12:01:30 +0700

    feat(technologies): refactor icon URL generation and update tech descriptions

--- src/lib/components/Technologies.svelte
+++ src/lib/components/Technologies.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
   import Section from "$components/Section.svelte";
   import config from "$lib/config";
+
+  const getIconUrl = (originalSlug: string, extendedSlug?: string) => {
+    if (!originalSlug) throw new Error("Original slug is required");
+
+    let finalSlug;
+
+    if (extendedSlug && extendedSlug.length > 0) {
+      finalSlug = extendedSlug;
+    } else {
+      finalSlug = `${originalSlug}-original`;
+    }
+
+
+    return `https://raw.githubusercontent.com/devicons/devicon/master/icons/${originalSlug}/${finalSlug}.svg`;
+  };
+
 </script>
 
 <Section id="technologies" title="Technologies">
@@ -10,7 +26,7 @@
         class="flex justify-start items-center px-3 py-2 bg-linear-to-r from-white/[0.095] to-white/5 border border-white/5 hover:border-white/10 rounded-xl"
       >
         <img
-          src="https://raw.githubusercontent.com/devicons/devicon/master/icons/{tech.slug}/{tech.slug}-original.svg"
+          src={getIconUrl(tech.slug, tech.extendedSlug)}
           alt={tech.name}
           class="size-7 mr-3"
         />
